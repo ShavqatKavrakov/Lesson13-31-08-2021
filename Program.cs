b23a45304bbd8a182d73ffc9d5d2af816a68f8db
ShavqatKavrakov <kavrakovshavqatinfo@gmail.com>
2021-09-01 20:57:20 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Lesson13_31_08_2021
+{
+interface IPlayable
+{
+    void Play();
+
+    void Pause();
+    void Stop();
+}
+interface IRecodable
+{
+    void Record();
+    void Pause();
+    void Stop();
+}
+class Player:IPlayable,IRecodable
+{
+  public void Play()=>System.Console.WriteLine("Play");
+  public void Pause()=>System.Console.WriteLine("Pause");
+  public void Stop()=>System.Console.WriteLine("Stop");
+  public void Record()=>System.Console.WriteLine("Record"); 
+}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player= new Player();
+            Play(player);
+        }
+        public static void Play(Player player)
+        {
+            player.Pause();
+            player.Play();
+            player.Record();
+            player.Stop();
+        }
+    }
+}
